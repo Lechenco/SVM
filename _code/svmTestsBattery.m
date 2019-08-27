a5c3fe6cad9d4a29442e9f9af973b3cefc1191b9
Lechenco <glechenco@gmail.com>
2019-08-27 10:00:26 -0300

    Wavelet Article

---
+++ _code/svmTestsBattery.m
@@ -0,0 +1,48 @@
+load('test.mat');
+kernel = 'rbf';
+runs = 30;
+
+% Characteristics columns
+characteristics = [1:24];
+
+runsAccuracy = [];
+runsPrecision = [];
+runsRecall = [];
+runsF1score = [];
+
+for i = 1:runs
+    data = svmData;
+    fprintf("Running %d of %d times...\n", i, runs);
+    
+    X = data(:,characteristics);
+    Y = data(:, 25);
+
+    posi = find(Y == 1);
+    negi = find(Y == -1);
+    posi = posi(randperm(length(posi)));
+    posi = posi(1:length(negi));
+    Y = [Y(negi); Y(posi)];
+    X = [X(negi,:); X(posi, :)];
+
+    %Prepare for training
+    trainId = separateTrainAndTest(Y, 0.8);
+    [Xtrain, Xpredict, Ytrain, Ypredict] = getTrainTestData(X, Y, trainId);
+
+    %Train
+    SVMModel = fitSVM(Xtrain, Ytrain, kernel, 0);
+    %Predict
+    [label,score] = predict(SVMModel,Xpredict);
+    % Metrics
+    [accuracy, precision, recall, f1score] = getMetrics(Ypredict, label);
+    
+    runsAccuracy = [runsAccuracy accuracy];
+    runsPrecision = [runsPrecision precision];
+    runsRecall = [runsRecall recall];
+    runsF1score = [runsF1score f1score];
+end
+    
+fprintf("Mean Accuracy: %.5f\n", mean(runsAccuracy));
+fprintf("Mean Precision: %.5f\n", mean(runsPrecision));
+fprintf("Mean Recall: %.5f\n", mean(runsRecall));
+fprintf("Mean F1score: %.5f\n", mean(runsF1score));
+    
